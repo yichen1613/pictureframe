4168a0177cfa5bf60bd2b383598c976029750ff5
Anselina Chia <anselina.chia@gmail.com>
2013-06-01 03:27:57 -0400

    Added User database classes (Closes #1)

---
+++ PictureFrame/src/ca/taglab/PictureFrame/database/UserTable.java
@@ -0,0 +1,38 @@
+package ca.taglab.PictureFrame.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class UserTable {
+
+    // Database table
+    public static final String TABLE_NAME = "User";
+    public static final String COL_ID = "_id";
+    public static final String COL_USERID = "uid";
+    public static final String COL_NAME = "name";
+    public static final String COL_PASSWORD = "password";
+    public static final String COL_IMG = "img";
+
+    // SQL statement for database creation
+    private static final String CREATE_USER_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_USERID + " INTEGER NOT NULL, "
+            + COL_NAME + " TEXT NOT NULL, "
+            + COL_PASSWORD + " TEXT NOT NULL, "
+            + COL_IMG + " TEXT NOT NULL"
+            + ");";
+
+    public static void onCreate(SQLiteDatabase db) {
+        Log.w(UserTable.class.getName(), "Creating database");
+        db.execSQL(CREATE_USER_TABLE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(UserTable.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion
+                + ". All existing data will be lost.");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
