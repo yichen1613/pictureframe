c45d6698123c815bbc12cf0dae44eac07aac3af4
Anselina Chia <anselina.chia@gmail.com>
2013-06-10 15:05:00 -0400

    Removed title bar

--- PictureFrame/src/ca/taglab/PictureFrame/MessageHistoryActivity.java
+++ PictureFrame/src/ca/taglab/PictureFrame/MessageHistoryActivity.java
@@ -10,16 +10,26 @@ import android.widget.*;
 public class MessageHistoryActivity extends ListActivity {
 
     static final String[] MESSAGES = new String[]
-            { "This is message 1. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 2. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 3. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 4. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 5. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 6. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 7. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 8. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 9. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
-              "This is message 10. Testing testing testing testing testing testing testing testing testing testing testing testing testing" };
+            { "This is message 1. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 2. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 3. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 4. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 5. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 6. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 7. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 8. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 9. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 10. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 11. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 12. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 13. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 14. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 15. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 16. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 17. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 18. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 19. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 20. Testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing testing" };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
