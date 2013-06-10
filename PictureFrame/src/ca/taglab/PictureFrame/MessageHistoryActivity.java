f5e079d4d69667cf652a9043034f2b3d4862055e
Anselina Chia <anselina.chia@gmail.com>
2013-06-10 13:34:52 -0400

    Added Message History screen

---
+++ PictureFrame/src/ca/taglab/PictureFrame/MessageHistoryActivity.java
@@ -0,0 +1,44 @@
+package ca.taglab.PictureFrame;
+
+import android.app.ListActivity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MessageHistoryActivity extends ListActivity {
+
+    static final String[] MESSAGES = new String[]
+            { "This is message 1. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 2. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 3. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 4. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 5. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 6. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 7. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 8. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 9. Testing testing testing testing testing testing testing testing testing testing testing testing testing",
+              "This is message 10. Testing testing testing testing testing testing testing testing testing testing testing testing testing" };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_message_history, MESSAGES));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        // Change colour of the divider line
+        listView.setDivider(new ColorDrawable(Color.parseColor("#57A5FF")));
+        listView.setDividerHeight(4);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
