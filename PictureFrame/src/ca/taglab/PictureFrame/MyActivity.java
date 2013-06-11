6fa67e66e186418e6ba8f1c651c90e455f296661
Steve Tsourounis <stevetsourounis@gmail.com>
2013-06-11 14:10:47 -0400

    Changed the main activity

--- PictureFrame/src/ca/taglab/PictureFrame/MyActivity.java
+++ PictureFrame/src/ca/taglab/PictureFrame/MyActivity.java
@@ -1,17 +1,38 @@
 package ca.taglab.PictureFrame;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import ca.taglab.PictureFrame.database.AddPicture;
 import ca.taglab.PictureFrame.database.DatabaseHelper;
 import ca.taglab.PictureFrame.database.UserTable;
 import ca.taglab.PictureFrame.email.MailSenderActivity;
 import ca.taglab.PictureFrame.provider.UserContentProvider;
 
-public class MyActivity extends Activity {
+public class MyActivity extends ListActivity {
+    private static Sample[] mSamples;
     DatabaseHelper db;
+
+
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
+
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
+        }
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
+    }
 
     /**
      * Called when the activity is first created.
@@ -21,34 +42,24 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ContentValues values = new ContentValues();
-        values.put(UserTable.COL_NAME, "Steve");
-        values.put(UserTable.COL_EMAIL, "steve@taglab.ca");
-        values.put(UserTable.COL_IMG, "steve.jpg");
-        values.put(UserTable.COL_PASSWORD, "5t3v3");
-        getContentResolver().insert(UserContentProvider.CONTENT_URI, values);
+        mSamples = new Sample[] {
+                //new Sample(R.string.title_log_in, )
+                new Sample(R.string.title_add_picture, AddPicture.class),
+                new Sample(R.string.title_activity_mail_sender, MailSenderActivity.class),
+                new Sample(R.string.title_activity_message_history, MessageHistoryActivity.class),
+                new Sample(R.string.title_view_gallery, ScreenSlideActivity.class)
+        };
 
-        db = new DatabaseHelper(getApplicationContext());
+        setListAdapter(new ArrayAdapter<Sample>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                mSamples));
 
-    }
-
-    public void addImage(View v) {
-        // Open activity
-        startActivity(new Intent(this, ScreenSlideActivity.class));
-    }
-
-    public void startMessageHistoryActivity(View view) {
-        Intent intent = new Intent(this, MessageHistoryActivity.class);
-        startActivity(intent);
-    }
-
-    public void startMailSenderActivity(View view) {
-        Intent intent = new Intent(this, MailSenderActivity.class);
-        startActivity(intent);
     }
 
     @Override
-    public void onDestroy() {
-        db.close();
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        // Launch the activity associated with this list position
+        startActivity(new Intent(MyActivity.this, mSamples[position].activityClass));
     }
 }
