a7df75aa699473da0355f9d53147099885ddac8d
Anselina Chia <anselina.chia@gmail.com>
2013-07-18 23:18:10 -0400

    Detect attachments in emails

--- PictureFrame/src/ca/taglab/PictureFrame/email/GmailReader.java
+++ PictureFrame/src/ca/taglab/PictureFrame/email/GmailReader.java
@@ -16,6 +16,8 @@ import javax.mail.Multipart;
 import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.mail.search.FlagTerm;
 
@@ -37,7 +39,7 @@ public class GmailReader {
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
         
-        Session session = Session.getDefaultInstance(props, null);
+        Session session = Session.getInstance(props, null);
         Log.d(TAG, session.toString());
         Store store = session.getStore("imaps");
         store.connect("imap.gmail.com", this.email, this.pwd);
@@ -78,6 +80,7 @@ public class GmailReader {
 
             Object msgBody = message.getContent();
             String msgBodyFinal = "";
+            
             if (msgBody instanceof String) {
                 // plain text message
                 Log.d(TAG, "PLAIN TEXT body: " + msgBody.toString());   
@@ -86,23 +89,54 @@ public class GmailReader {
                 // multipart message
                 Multipart mp = (Multipart) msgBody;
                 for (int j = 0; j < mp.getCount(); j++) {
-                    BodyPart bp = mp.getBodyPart(j);
+                    Part bp = mp.getBodyPart(j);
                     String disposition = bp.getDisposition();
                     
-                    if (disposition != null && (disposition.equalsIgnoreCase(BodyPart.ATTACHMENT))) {
-                        Log.d(TAG, "Email has attachment");
-                        DataHandler handler = bp.getDataHandler();
-                        Log.d(TAG, "Attachment filename: " + handler.getName());
+                    if (disposition != null) {
+                        if (disposition.equalsIgnoreCase(Part.ATTACHMENT) || disposition.equalsIgnoreCase(Part.INLINE)) {
+                            Log.d(TAG, "Disposition != null");
+                            String filename = bp.getFileName();
+                            Log.d(TAG, "******Email has attachment******");
+                            //DataHandler handler = bp.getDataHandler();
+                            //Log.d(TAG, "Attachment filename: " + handler.getName());
+                            Log.d(TAG, "Attachment filename: " + filename);
+                        }
                     } else {
-                        Log.d(TAG, "MULTIPART body #" + j + ": " + bp.getContent().toString());
-                        if (j == 0) {  // actual message (no HTML tags, etc.)
+                        // Handle cases where message parts are NOT flagged appropriately
+                        Log.d(TAG, "Disposition == null, so check isMimeType");
+                        // check if plain
+                        MimeBodyPart mbp = (MimeBodyPart) bp;
+                        if (mbp.isMimeType("text/plain")) {
+                            // Handle plain
+                            Log.d(TAG, "isMimeType #" + j + ": text/plain");
+                            Log.d(TAG, "MULTIPART body #" + j + ": " + bp.getContent().toString());
                             msgBodyFinal = bp.getContent().toString();
+                        } else if (mbp.isMimeType("text/html")) {
+                            Log.d(TAG, "isMimeType #" + j + ": text/html");
+                        } else if (mbp.isMimeType("text/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": text/*");
+                        } else if (mbp.isMimeType("message/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": message/*");   
+                        } else if (mbp.isMimeType("image/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": image/*");
+                        } else if (mbp.isMimeType("video/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": video/*");
+                        } else if (mbp.isMimeType("audio/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": audio/*");
+                        
+                        } else if (mbp.isMimeType("application/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": application/*");
+                        } else if (mbp.isMimeType("model/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": model/*");
+                        } else if (mbp.isMimeType("multipart/*")) {
+                            Log.d(TAG, "isMimeType #" + j + ": multipart/*");
+                            //processMessage(bp.getContent());
+                        }
+                        else {
+                            Log.d(TAG, "isMimeType #" + j + " DID NOT MATCH! getContentType(): " + mbp.getContentType());
                         }
                     }
                 }
-            } else {
-                // unrecognized message type
-                Log.d(TAG, "Unrecognized message body type");
             }
 
             Msg msg = this.new Msg(msgNum, msgDate, msgFrom, msgSubject, msgBodyFinal);
