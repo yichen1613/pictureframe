f1faa68422f7793538cf51731e6fa39a9f0be4bb
Anselina Chia <anselina.chia@gmail.com>
2013-07-30 14:39:01 -0400

    Modify MessageTable to store received emails

--- PictureFrame/src/ca/taglab/PictureFrame/database/MessageTable.java
+++ PictureFrame/src/ca/taglab/PictureFrame/database/MessageTable.java
@@ -7,20 +7,22 @@ public class MessageTable {
 
     public static final String TABLE_NAME = "Message";
     public static final String COL_ID = "_id";
-    public static final String COL_CONTENT = "content";
-    public static final String COL_DATE = "date";
     public static final String COL_TO_ID = "to_id";
     public static final String COL_FROM_ID = "from_id";
-    public static final String COL_TYPE_ID = "type_id";
+    public static final String COL_DATE = "date";
+    public static final String COL_SUBJECT = "subject";
+    public static final String COL_BODY = "body";
+    public static final String COL_ATTACHMENTS = "attachments";
 
     private static final String CREATE_MESSAGE_TABLE =
             "CREATE TABLE " + TABLE_NAME + " ("
             + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COL_CONTENT + " TEXT NOT NULL, "
             + COL_DATE + " TEXT NOT NULL, "
-            + COL_TO_ID + " INTEGER NOT NULL, "
-            + COL_FROM_ID + " INTEGER NOT NULL, "
-            + COL_TYPE_ID + " INTEGER NOT NULL"
+            + COL_SUBJECT + " TEXT NOT NULL, "
+            + COL_BODY + " TEXT, "
+            + COL_ATTACHMENTS + "TEXT, "
+            + COL_TO_ID + " INTEGER, "
+            + COL_FROM_ID + " INTEGER NOT NULL"
             + ");";
 
     public static void onCreate(SQLiteDatabase db) {
