60771aa8256d25234fdbbacc7bf9b6ae40746666
Anselina Chia <anselina.chia@gmail.com>
2013-07-31 23:21:24 -0400

    Modify MessageTable to include Type column

--- PictureFrame/src/ca/taglab/PictureFrame/database/MessageTable.java
+++ PictureFrame/src/ca/taglab/PictureFrame/database/MessageTable.java
@@ -9,18 +9,18 @@ public class MessageTable {
     public static final String COL_ID = "_id";
     public static final String COL_TO_ID = "to_id";
     public static final String COL_FROM_ID = "from_id";
-    public static final String COL_DATE = "date";
+    public static final String COL_DATETIME = "date";
     public static final String COL_SUBJECT = "subject";
     public static final String COL_BODY = "body";
-    public static final String COL_ATTACHMENTS = "attachments";
+    public static final String COL_TYPE = "type";
 
     private static final String CREATE_MESSAGE_TABLE =
             "CREATE TABLE " + TABLE_NAME + " ("
             + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COL_DATE + " TEXT NOT NULL, "
-            + COL_SUBJECT + " TEXT NOT NULL, "
+            + COL_TYPE + " TEXT NOT NULL, "
+            + COL_DATETIME + " TEXT NOT NULL, "
+            + COL_SUBJECT + " TEXT, "
             + COL_BODY + " TEXT, "
-            + COL_ATTACHMENTS + "TEXT, "
             + COL_TO_ID + " INTEGER, "
             + COL_FROM_ID + " INTEGER NOT NULL"
             + ");";
