ae37e82aa9094b54ac12e3bc69229c34919e098f
Anselina Chia <anselina.chia@gmail.com>
2013-08-08 12:07:38 -0400

    Add validation of email address

--- PictureFrame/src/ca/taglab/PictureFrame/LoginActivity.java
+++ PictureFrame/src/ca/taglab/PictureFrame/LoginActivity.java
@@ -17,6 +17,8 @@ import ca.taglab.PictureFrame.database.UserTable;
 import ca.taglab.PictureFrame.email.ReadEmailAsyncTask;
 import ca.taglab.PictureFrame.provider.UserContentProvider;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -50,31 +52,39 @@ public class LoginActivity extends Activity {
         e.putString("password", mPassword);
         
         if (!mEmail.isEmpty() && !mPassword.isEmpty()) {
-            e.commit();
-            
-            int uid = queryForUserId(mEmail);
-            if (uid == 0) {
-                // User does not exist in UserTable, so insert the user
-                ContentValues values = new ContentValues();
-                values.put(UserTable.COL_NAME, "User");
-                values.put(UserTable.COL_EMAIL, mEmail);
-                values.put(UserTable.COL_IMG, "none");
-                values.put(UserTable.COL_PASSWORD, "1234");
-                getContentResolver().insert(UserContentProvider.USER_CONTENT_URI, values);
-            } else {
-                // do nothing (since nothing needs to be updated)
-            }
-            
-            Toast.makeText(this, "Retrieving unread emails...", Toast.LENGTH_LONG);
-            Log.d(TAG, "Retrieving unread emails...");
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                public void run() {
-                    getUnreadEmails();
+            if (isValidEmail(mEmail)) {
+                e.commit();
+
+                int uid = queryForUserId(mEmail);
+                if (uid == 0) {
+                    // User does not exist in UserTable, so insert the user
+                    ContentValues values = new ContentValues();
+                    values.put(UserTable.COL_NAME, "User");
+                    values.put(UserTable.COL_EMAIL, mEmail);
+                    values.put(UserTable.COL_IMG, "none");
+                    values.put(UserTable.COL_PASSWORD, "1234");
+                    getContentResolver().insert(UserContentProvider.USER_CONTENT_URI, values);
+                } else {
+                    // do nothing (since nothing needs to be updated)
                 }
-            }, 0, REFRESH_INTERVAL);
-            
-            finish();
+
+                Toast.makeText(this, "Retrieving unread emails...", Toast.LENGTH_LONG);
+                Log.d(TAG, "Retrieving unread emails...");
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    public void run() {
+                        getUnreadEmails();
+                    }
+                }, 0, REFRESH_INTERVAL);
+
+                finish();
+            } else {
+                Toast toast = Toast.makeText(this, "Invalid email address", Toast.LENGTH_LONG);
+                LinearLayout toastLayout = (LinearLayout) toast.getView();
+                TextView toastTV = (TextView) toastLayout.getChildAt(0);
+                toastTV.setTextSize(30);
+                toast.show();
+            }
         } else {
             Toast toast = Toast.makeText(this, "Blank fields are not allowed", Toast.LENGTH_LONG);
             LinearLayout toastLayout = (LinearLayout) toast.getView();
@@ -109,6 +119,20 @@ public class LoginActivity extends Activity {
 
         mCursor.close();
         return uid;
+    }
+
+    /**
+     * Check if the email address is valid
+     */
+    public static boolean isValidEmail(String email) {
+        boolean isValid = true;
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+        } catch (AddressException ex) {
+            isValid = false;
+        }
+        return isValid;
     }
     
 }
